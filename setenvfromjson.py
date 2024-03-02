e15b3d9f34c0ca3dcb5dd89d95d19ae6100fe33c
“vamc-krish-git“ <ismav.pandu@gmail.com>
2024-03-02 23:22:53 +0100

    Set GitHub Env from json

---
+++ setenvfromjson.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+# Path to the JSON file
+json_file_path = 'input.json'
+
+# Read JSON file
+with open(json_file_path, 'r') as file:
+    json_data = json.load(file)
+
+# # Print key-value pairs
+# for key, value in json_data.items():
+#     print(f"Key: {key}, Value: {value}")
+
+# Set each key as environment variable
+for key, value in json_data.items():
+    os.environ[key] = str(value)
+
+# Print environment variables
+for key, value in os.environ.items():
+    print(f"Environment Variable: {key}, Value: {value}")
