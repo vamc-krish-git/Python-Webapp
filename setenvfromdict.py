a33e54e33218b563539b466664bcb12d84043c14
“vamc-krish-git“ <ismav.pandu@gmail.com>
2024-03-03 19:33:24 +0100

    Read from Dictionary

---
+++ setenvfromdict.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+# Command line argument: key1:value1,key2:value2
+custom_string = sys.argv[1]
+
+# Parse the custom string into a dictionary
+my_dict = dict(item.split(":") for item in custom_string.split(","))
+
+print(my_dict)
+
+for key, value in my_dict.items():
+    os.environ[key] = value
+
